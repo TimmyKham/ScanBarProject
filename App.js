5fe120b05655259ee0709bcec0935314d4333b83
TimmyKham <tkhamsithideth@gmail.com>
2020-08-25 16:11:06 +0200

    Adding Camera scan barcode

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Camera from './screens/Camera';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Camera" component={Camera} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
